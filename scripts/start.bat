05ffbb036f5567def079bba4aade28b65093c710
Jonathan Harper <39912347+jharper-sec@users.noreply.github.com>
2024-02-01 17:29:43 -0500

    Added polling to start scripts

--- scripts/start.bat
+++ scripts/start.bat
@@ -2,7 +2,6 @@
 SETLOCAL ENABLEDELAYEDEXPANSION
 
 :: Function to check if a port is in use
-:: Syntax: call :is_port_in_use [port_number]
 :is_port_in_use
 netstat -ano | findstr :%1 | findstr LISTENING > nul
 if %ERRORLEVEL% == 0 (
@@ -11,6 +10,19 @@ if %ERRORLEVEL% == 0 (
 ) else (
     exit /b 0
 )
+
+:: Function to wait for server readiness
+:wait_for_server
+SET PORT=%1
+:server_poll_loop
+powershell -Command "(New-Object Net.Sockets.TcpClient).Connect('localhost', %PORT%)" > nul 2>&1
+IF NOT %ERRORLEVEL% == 0 (
+    echo Waiting for server on port %PORT%...
+    TIMEOUT /T 2 /NOBREAK > nul
+    GOTO server_poll_loop
+)
+echo Server on port %PORT% is ready!
+exit /b 0
 
 :: Check if the contrast_security.yaml file exists
 IF NOT EXIST contrast_security.yaml (
@@ -27,6 +39,8 @@ start /b cmd /c java -Dcontrast.protect.enable=false -Dcontrast.assess.enable=tr
     -Dcontrast.config.path=contrast_security.yaml ^
     -javaagent:contrast-agent.jar -jar terracotta.war > terracotta-dev.log
 
+call :wait_for_server 8080
+
 :: Start the application in PRODUCTION mode (Protect)
 call :is_port_in_use 8082
 IF %ERRORLEVEL% == 1 exit /b 1
@@ -35,6 +49,8 @@ start /b cmd /c java -Dcontrast.protect.enable=true -Dcontrast.assess.enable=fal
     -Dcontrast.server.environment=PRODUCTION -Dserver.port=8082 ^
     -Dcontrast.config.path=contrast_security.yaml ^
     -javaagent:contrast-agent.jar -jar terracotta.war > terracotta-prod.log
+
+call :wait_for_server 8082
 
 echo Both servers started.
 exit /b 0
--- scripts/start.sh
+++ scripts/start.sh
@@ -6,6 +6,17 @@ is_port_in_use() {
     return $?
 }
 
+# Function to check if the application is ready
+wait_for_server() {
+    PORT=$1
+    echo "Waiting for server on port $PORT to be ready..."
+    while ! curl --output /dev/null --silent --head --fail http://localhost:"$PORT"; do
+        printf '.'
+        sleep 2
+    done
+    echo "Server on port $PORT is ready!"
+}
+
 # Check if the contrast_security.yaml file exists
 CONFIG_FILE="contrast_security.yaml"
 if [ ! -f "$CONFIG_FILE" ]; then
@@ -24,7 +35,7 @@ else
     -Dcontrast.server.environment=DEVELOPMENT -Dserver.port=$DEV_PORT \
     -Dcontrast.config.path=$CONFIG_FILE \
     -javaagent:contrast-agent.jar -jar terracotta.war > $DEV_LOG 2>&1 &
-    echo "Development server started on port $DEV_PORT"
+    wait_for_server $DEV_PORT
 fi
 
 # Start the application in PRODUCTION mode (Protect)
@@ -38,5 +49,5 @@ else
     -Dcontrast.server.environment=PRODUCTION -Dserver.port=$PROD_PORT \
     -Dcontrast.config.path=$CONFIG_FILE \
     -javaagent:contrast-agent.jar -jar terracotta.war > $PROD_LOG 2>&1 &
-    echo "Production server started on port $PROD_PORT"
+    wait_for_server $PROD_PORT
 fi
